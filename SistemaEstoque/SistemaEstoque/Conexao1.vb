cdb0704580d885cff58102c709e98d3b90f32c43
xDavex <dave.use@gmail.com>
2016-11-04 18:39:36 -0200

    Arquivos

---
+++ SistemaEstoque/SistemaEstoque/Conexao1.vb
@@ -0,0 +1,27 @@
+﻿Module Conexao1
+
+    Public db As New ADODB.Connection
+    Public rs As New ADODB.Recordset
+    Public sql As String
+    Public conexao = Application.StartupPath & "\db.mdb"
+
+    Sub conexao_banco()
+        Try
+            db = CreateObject("ADODB.Connection")
+            db.Open("Provider=Microsoft.JET.OLEDB.4.0;Data Source=" & Conexao)
+            'db.Open("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & conexao)'
+        Catch ex As Exception
+            MsgBox("Conexão falhou!" & ex.Message)
+        End Try
+    End Sub
+'teste de conexão
+'mais linhas
+'adicionadas
+    Sub limpar_login()
+        With Login
+            .txt_senha.Clear()
+            .txt_usu.Clear()
+            .lbl_tent.Visible = False
+        End With
+    End Sub
+End Module
---
+++ SistemaEstoque/SistemaEstoque/frm_esqueceuSenha.vb
@@ -0,0 +1,20 @@
+﻿Public Class frm_esqueceuSenha
+
+    Private Sub btn_confirmar_Click(sender As Object, e As EventArgs) Handles btn_confirmar.Click
+        sql = "select * from tb_usuario where NomeUsu ='" & txt_usu.Text & "' and RespostaUsu='" & txt_resposta.Text & "'"
+        Try
+            rs = db.Execute(sql)
+            If rs.EOF = False Then
+                MsgBox("Senha da conta: '" & rs.Fields(7).Value & "'", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "Aviso")
+                Me.Dispose()
+            Else
+                MsgBox("Resposta ou usuário incorreto!")
+                txt_resposta.Clear()
+                txt_resposta.Focus()
+            End If
+        Catch ex As Exception
+            MsgBox("Erro! \nDesc:" & ex.Message)
+        End Try
+    End Sub
+
+End Class
